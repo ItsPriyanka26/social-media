1a42aaab3ef871f48e9ea48b1fb3cb9badf62027
Priyanka Gupta <75130551+ItsPriyanka26@users.noreply.github.com>
2023-01-06 10:38:04 +0530

    Add files via upload

---
+++ postsController.js
@@ -0,0 +1,10 @@
+
+
+const getAllPostsController = async(req, res) => {
+    console.log(req._id);
+    return res.send("These are all the posts");
+}
+
+module.exports = {
+    getAllPostsController
+};
